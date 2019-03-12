264bdfed56c54d933b34fbe1a0a20fcd44902a41
Rigaudeau Léo <leo.rigaudeau@ynov.com>
2019-03-12 09:23:51 +0100

    debut des models

--- models/bieres.js
+++ models/bieres.js
@@ -1,7 +1,21 @@
 const { mongoose } = require('../db/connectdb.js');
 
 const Biere = mongoose.model('Biere', {
-  
+    nom: {
+        type: String,
+        required: true
+    },
+    Alcool: {
+        type: String
+    },
+    Brasseur: [{
+        type: Schema.Types.ObjectId,
+        ref: 'brasseur'
+    }],
+    Origine: [{
+        type: Schema.Types.ObjectId,
+        ref: 'pays'
+    }],
 });
 
 module.exports = { Biere };
